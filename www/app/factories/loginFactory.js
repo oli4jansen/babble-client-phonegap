bdab37e35bbf26412cace9af8cdc943e5ebb3c47
Olivier Jansen <oli4jansen.nl@gmail.com>
2014-03-31 00:22:50 +0200

    Comments bij loginFactory gezet

--- www/app/factories/loginFactory.js
+++ www/app/factories/loginFactory.js
@@ -1,12 +1,13 @@
 app.factory('loginFactory', function($http, $location, $window) {
 
+	// API URL
 	var URL = 'http://192.168.2.14';
 
+	// Facebook SDK variabele
 	var FB = $window.FB;
-	if(!FB) {
-		alert('FB not loaded');
-	}
+	if(!FB) alert('FB not loaded');
 
+	// Deze factory is een object
 	var factory = {};
 
 	// Boolean die aangeeft of er ingelogd is
@@ -25,14 +26,19 @@ app.factory('loginFactory', function($http, $location, $window) {
 	factory.userId;
 	factory.userInfo = [];
 
+	/*
+	 *	Facebook SDK functions
+	 */
+
+	// Inloggen bij Facebook
 	factory.logIn = function() {
-		// Inloggen
 		FB.login(
 	        function(response) {
 	            if (response.authResponse) {
+	            	// Als het gelukt is; herlaad de 'pagina'/app
 	            	location.reload();
 	            }else{
-	                alert('Niet ingelogd');
+	            	// Gebruiker cancelde het login scherm
 	                return;
 	            }
 	        },
@@ -40,39 +46,75 @@ app.factory('loginFactory', function($http, $location, $window) {
 	    );
 	};
 
+	// Uitloggen bij Facebook
 	factory.logOut = function() {
 		FB.logout(function(response) {
 			location.reload();
 		});
 	};
 
-	factory.authenticate = function(data) {
+	/*
+	 *	Babble API functions
+	 */
 
+	// Authenticate een gebruiker bij de API
+	// Verplicht in data: { accessToken: (..) }
+	factory.authenticate = function(data) {
+		// POST de data naar het API endpoint
 		$http.post(URL + '/user/authenticate', data).success(function(data) {
 			if(data.status === 200) {
+				// Status 200 betekent:
+				//   -  gebruiker bestaat en accessToken is geldig en bijgewerkt
+				//   -  gebruiker bestond niet maar is aangemaakt met de geleverde data
+
+				// Er is ingelogd
 				factory.loggedIn 			= true;
+
+				// Het profiel bestaat in de database
 				factory.profileCompleted 	= true;
+
+				// Het userID zoals we die terugkregen van de server
 				factory.userId 				= data.data.id;
+
+				// Alle info van de gebruiker ophalen en instellen in de factory zodat deze beschikbaar is voor verschillende controllers
 				factory.getUserInfo(function(err, data) {
-					if(err) alert(err);
-					factory.userInfo = data;
-					$location.path( "/feed" );
+					if(err) {
+						alert(err);
+					}else{
+						factory.userInfo = data;
+						// Shit is geregeld; doorsturen naar de feed.
+						$location.path( "/feed" );
+					}
 				});
+
 			}else if(data.status === 206) {
+				// Status 206 betekent: gebruiker bestaat niet en er was alleen een accessToken aanwezig in de data
+
+				// Er is ingelogd
 				factory.loggedIn 			= true;
+
+				// Het profiel bestaat echter nog niet in de database
 				factory.profileCompleted 	= false;
+
+				// Het userID zoals we die terugkregen van de server
 				factory.userId 				= data.data.id;
+
+				// De data die al wel bekend is van de gebruiker
 				factory.userPartialData 	= data.data;
+
+				// Doorsturen naar een scherm om extra info in te voeren
 				$location.path('/completeProfile');
 			}else{
-				alert(JSON.stringify(data));
+				// Dit antwoord kennen we niet van de server, er is iets mis.
 				alert('Something went wrong while authenticating.');
 			}
 		}).error(function(data){
+			// Verbinding maken met API is mislukt
 			alert('Couldn\'t connect to the API server.');
 		});
 	};
 
+	// Gebruikers info ophalen bij de API
 	factory.getUserInfo = function(callback) {
 		$http.get(URL + '/user/'+this.userId).success(function(data) {
 			callback(false, data.data[0]);
@@ -81,6 +123,7 @@ app.factory('loginFactory', function($http, $location, $window) {
 		});
 	};
 
+	// Informatie over een match tussen 2 personen ophalen
 	factory.getMatchInfo = function(matchId, callback) {
 		$http.get(URL + '/user/'+this.userId+'/match/'+matchId).success(function(data) {
 			callback(false, data.data[0]);
@@ -89,6 +132,7 @@ app.factory('loginFactory', function($http, $location, $window) {
 		});
 	};
 
+	// Alle matches van 1 persoon ophalen
 	factory.getMatches = function(callback) {
 		$http.get(URL + '/user/'+this.userId+'/matches').success(function(data) {
 			callback(false, data.data);
@@ -97,6 +141,7 @@ app.factory('loginFactory', function($http, $location, $window) {
 		});
 	};
 
+	// Account verwijderen
 	factory.deleteAccount = function(callback) {
 		$http.delete(URL + '/user/'+this.userId).success(function(data) {
 			callback(false, data);
@@ -105,27 +150,25 @@ app.factory('loginFactory', function($http, $location, $window) {
 		});
 	};
 
-	factory.refreshUserInfo = function() {
-		factory.getUserInfo(function(err, data) {
-			if(!err){
-				factory.userInfo = data;
-			}else{
-				alert('Data refresh error '+err);
-			}
-		});
-	};
-
+	// De user info updaten met nieuwe gegevens
 	factory.updateUserInfo = function(data, callback) {
 		try {
+			// Object met data die we naar de server gaan sturen
 			var parsedData = {};
+
+			// Data is een array, dus voor elke row uit data:
 			for(var i=0;i<data.length;i++) {
-				//console.log(formData[i].name);
+				// Een item aan parsedData toevoegen
 				parsedData[data[i].name] = data[i].value;
 			}
+
+			// Checkboxes verdwijenen als je ze niet checkt
 			if(!parsedData.likeMen) parsedData.likeMen = '0';
 			if(!parsedData.likeWomen) parsedData.likeWomen = '0';
 
+			// Als alles klopt, kunnen we PUTten
 			if(parsedData.description !== undefined && parsedData.likeMen !== undefined && parsedData.likeWomen !== undefined && parsedData.searchRadius !== undefined) {
+				// PUT nieuwe data
 				$http.put(URL + '/user/'+this.userId, parsedData).success(function(data) {
 					factory.refreshUserInfo();
 					callback(0, data);
@@ -140,15 +183,31 @@ app.factory('loginFactory', function($http, $location, $window) {
 		}
 	};
 
+	/*
+	 *	Overige functies
+	 */
+
+	// Functie om het verversen van de user info in deze factory van buitenaf makkelijker te maken
+	factory.refreshUserInfo = function() {
+		factory.getUserInfo(function(err, data) {
+			if(!err){
+				factory.userInfo = data;
+			}else{
+				alert('Data refresh error '+err);
+			}
+		});
+	};
+
+	// Als Facebook een login detecteert, kunnen we de authenticate functie uitvoeren
 	FB.Event.subscribe('auth.login', function(response) {
+		// AccessToken instellen
 		factory.accessToken = response.authResponse.accessToken;
+
+		// Authenticate gebruiker met accessToken
 		factory.authenticate({ accessToken: response.authResponse.accessToken });
 	});
 
-	FB.Event.subscribe('auth.logout', function(response) {
-		alert('auth.logout event');
-	});
-
+	// Facebook SDK instellen met ons Facebook App ID
 	try {
 		FB.init({ appId: "1432398570331786", nativeInterface: CDV.FB, useCachedDialogs: false });
 	} catch (e) {
