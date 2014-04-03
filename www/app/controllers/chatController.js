4baebfa4b6747b3dbf7b89a23f642cc446bd1ed6
Olivier Jansen <oli4jansen.nl@gmail.com>
2014-04-03 22:40:00 +0200

    Websocket URL fix

--- www/app/controllers/chatController.js
+++ www/app/controllers/chatController.js
@@ -1,6 +1,6 @@
 app.controller("chatController", function($scope, $route, $routeParams, $location, loginFactory, $rootScope){
 
-	var URL = 'http://www.oli4jansen.nl:81';
+	var URL = 'www.oli4jansen.nl:81';
 
 	$scope.herId = $routeParams.userId;
 	$scope.herName = $routeParams.userName;
