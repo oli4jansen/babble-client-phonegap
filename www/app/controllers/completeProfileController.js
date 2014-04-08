16919b0374991e9613526cf692cf17f2cc6de9f6
Olivier Jansen <oli4jansen.nl@gmail.com>
2014-04-08 19:47:37 +0200

    Weer een typo

--- www/app/controllers/completeProfileController.js
+++ www/app/controllers/completeProfileController.js
@@ -43,7 +43,9 @@ app.controller("completeProfileController", function($scope, $location, loginFac
 
 	$scope.pickDate = function() {
 		datePicker.show({ date: new Date(), mode: 'date', allowFutureDates: false }, function(date){
-			$scope.chosenData = date;
+			alert(date);
+			$scope.chosenDate = date;
+			alert($scope.chosenDate);
 		});
 	};
 
