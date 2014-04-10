54a4b9f34d532ac17f316d85d801ce47178b0e6e
Olivier Jansen <oli4jansen.nl@gmail.com>
2014-04-10 18:57:31 +0200

    See prev

--- www/app.js
+++ www/app.js
@@ -78,7 +78,8 @@ app.directive('personCard', function() {
 		},
 		template: '<h2>{{card.name}}, {{card.age}}</h2><p>{{card.description}}</p><span class="half-width"><i class="ion-ios7-navigate-outline"></i> {{card.distance}}</span><span class="half-width"><i class="ion-ios7-people-outline"></i> {{card.mutualFriends.length}} mutual</span>',
 		link: function(scope, element, attrs) {
-				scope.$apply(attrs.enter);
+			scope.$apply(attrs.likeFunction);
+			scope.$apply(attrs.dislikeFunction);
 //			scope.$watch(attrs.myDirective, function(value) {
 				// Pep binden aan het element
 				element.pep({
