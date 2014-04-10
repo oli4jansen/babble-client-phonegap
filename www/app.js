29edc4bba2c76bdc1b1e3d9e6bbfaafce1ad697b
Olivier Jansen <oli4jansen.nl@gmail.com>
2014-04-10 17:18:20 +0200

    typo

--- www/app.js
+++ www/app.js
@@ -76,11 +76,11 @@ app.directive('babbleDraggable', function() {
 		scope: {
 			like: '&likeFunction',
 			dislike: '&dislikeFunction',
-			card: '=card'
+			card: '=personInfo'
 		},
 		template: '<h2>{{card.name}}, {{card.age}}</h2><p>{{card.description}}</p><span class="half-width"><i class="ion-ios7-navigate-outline"></i> {{card.distance}}</span><span class="half-width"><i class="ion-ios7-people-outline"></i> {{card.mutualFriends.length}} mutual</span>',
 		link: function(scope, element, attrs) {
-			scope.$watch(attrs.myDirective, function(value) {
+//			scope.$watch(attrs.myDirective, function(value) {
 				// Pep binden aan het element
 				element.pep({
 //					useCSSTranslation: false,
@@ -120,7 +120,7 @@ app.directive('babbleDraggable', function() {
 
 					}
 				});
-			});
+		//	});
 			scope.$on('$destroy', function() {
 				// Unbind pep van het element
 				$.pep.unbind(element);
