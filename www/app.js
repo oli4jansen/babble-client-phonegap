07ef8ed4c549b046b367e192fc0a3996ed0bfdf9
Olivier Jansen <oli4jansen.nl@gmail.com>
2014-04-10 19:36:57 +0200

    More fixes

--- www/app.js
+++ www/app.js
@@ -76,7 +76,7 @@ app.directive('personCard', function() {
 		scope: {
 			card: '=personInfo'
 		},
-		template: '<h2>{{card.name}}, {{card.age}}</h2><p>{{card.description}}</p><span class="half-width"><i class="ion-ios7-navigate-outline"></i> {{card.distance}}</span><span class="half-width"><i class="ion-ios7-people-outline"></i> {{card.mutualFriends.length}} mutual</span>',
+		template: '<h2>{{card.name}}, {{card.age}}</h2><p>{{card.id}}</p><p>{{card.description}}</p><span class="half-width"><i class="ion-ios7-navigate-outline"></i> {{card.distance}}</span><span class="half-width"><i class="ion-ios7-people-outline"></i> {{card.mutualFriends.length}} mutual</span>',
 		link: function(scope, element, attrs) {
 //			scope.$watch(attrs.myDirective, function(value) {
 				// Pep binden aan het element
