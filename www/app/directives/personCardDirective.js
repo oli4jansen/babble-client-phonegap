841a079dcfdb4c1e01b2ec7d84503566ef158764
Olivier Jansen <oli4jansen.nl@gmail.com>
2014-04-12 23:20:24 +0200

    Transitions proberen

--- www/app/directives/personCardDirective.js
+++ www/app/directives/personCardDirective.js
@@ -9,18 +9,12 @@ app.directive('personCard', function() {
         // Pep binden aan het element
         element.pep({
           cssEaseDuration: 350,
+          cssEaseString: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)',
           revert: true,
-//					shouldPreventDefault: true,
-//					allowDragEventPropagation: false,
           start: function(ev, obj){
             obj.noCenter = false;
           },
           drag: function(ev, obj){
-
-//            var vel = obj.velocity();
-//            var rot = (vel.x)/10;
-//						scope.rotate(obj.$el, rot);
-
             if(obj.pos.x - obj.initialPosition.left > 100) {
               $('.like-buttons .like').addClass('selected');
               $('.like-buttons .dislike').removeClass('selected');
@@ -33,7 +27,6 @@ app.directive('personCard', function() {
             }
           },
           stop: function(ev, obj){
-//						scope.rotate(obj.$el, 0);
             $('.like-buttons .like').removeClass('selected');
             $('.like-buttons .dislike').removeClass('selected');
 
@@ -42,13 +35,10 @@ app.directive('personCard', function() {
             if(vel.x > 300 || (obj.pos.x - obj.initialPosition.left > 60)) {
               scope.$parent.$parent.like();
               scope.$apply();
-//							obj.$el.fadeOut(100);
             }else if(vel.x < -300 || (obj.pos.x - obj.initialPosition.left < -60)) {
               scope.$parent.$parent.dislike();
               scope.$apply();
-//							obj.$el.fadeOut(100);
             }
-
           }
         });
       scope.$on('$destroy', function() {
