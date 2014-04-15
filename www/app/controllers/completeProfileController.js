df59ffca5407034dbc7fd1ac671cc37e277b3a89
Olivier Jansen <oli4jansen.nl@gmail.com>
2014-04-15 21:47:31 +0200

    Added image picker to complete profile

--- www/app/controllers/completeProfileController.js
+++ www/app/controllers/completeProfileController.js
@@ -47,6 +47,63 @@ app.controller("completeProfileController", function($scope, $location, loginFac
 		});
 	};
 
+
+	$scope.selectPicture = function() {
+		window.imagePicker.getPictures(
+			function(results) {
+				for (var i = 0; i < results.length; i++) {
+
+					$scope.pictures.push({ url: results[i] });
+
+					// upload
+					loginFactory.uploadPicture(results[i], function(err) {
+						if(!err) {
+							// Als dit de laatste foto was:
+							if(i === results.length) {
+								var picturesMirror = [];
+
+								for (var j = 0; j < $scope.pictures.length; j++) {
+									if($scope.pictures[j].local) {
+										var fileNameIndex = results[i].lastIndexOf("/") + 1;
+										var fileName = results[i].substr(fileNameIndex);
+
+										picturesMirror.push({ url: 'http://www.oli4jansen.nl:81/profile-pictures/'+loginFactory.userId+'-'+fileName });
+									}else{
+										picturesMirror.push({ url: $scope.pictures[j].url });
+									}
+								}
+
+								loginFactory.updatePictureList(picturesMirror, function(err, data){
+									if(err) navigator.notification.alert(err, function(){return;}, 'Error!');
+								});
+							}else{
+								alert('Niet de laatste foto.');
+							}
+						}else{
+							console.log(err);
+							navigator.notification.alert('We\'re sorry but we couldn\'t upload your pictures.', function(){return;}, 'Couldn\'t upload.');
+						}
+					});
+
+					$scope.$apply();
+				}
+
+			}, function (error) {
+				alert('Couldn\'t get your photo.');
+			}, {
+				maximumImagesCount: 5 - $scope.pictures.length,
+				width: 300,
+				height: 400
+			}
+		);
+	};
+
+	$scope.removePicture = function(index) {
+		$scope.pictures.splice(index, 1);
+		$scope.$apply;
+	};
+
+
 	$scope.deleteAccount = function() {
 		if(confirm('Are you sure you want to delete your account and sign out?')){
 			loginFactory.logOut();
