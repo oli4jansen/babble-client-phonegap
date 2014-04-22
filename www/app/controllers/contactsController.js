aa42c9670f380371dedd09f774561716d00762c5
Olivier Jansen <oli4jansen.nl@gmail.com>
2014-04-22 20:18:55 +0200

    Fixes

--- www/app/controllers/contactsController.js
+++ www/app/controllers/contactsController.js
@@ -9,9 +9,9 @@ app.controller("contactsController", function($scope, $location, loginFactory){
 
 			data.forEach(function(item) {
 				if(item.action < 26) {
-					item.picture = 'img/unknown.png';
+					item.pictureStyle = 'background: url("img/unknown.png")';
 				}else{
-					item.picture = JSON.parse(item.pictureList)[0].url;
+					item.pictureStyle = 'background: url("'+JSON.parse(item.pictureList)[0].url+'")';
 				}
 			});
 			$scope.matches = data;
