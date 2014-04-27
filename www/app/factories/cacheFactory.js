2055167bc2734f8fa4798191a84649ff972e2964
Olivier Jansen <oli4jansen.nl@gmail.com>
2014-04-28 01:23:17 +0200

    Cache factory changes

--- www/app/factories/cacheFactory.js
+++ www/app/factories/cacheFactory.js
@@ -4,17 +4,20 @@ app.factory('cacheFactory', function($location, $window, $sce, $q, $http) {
 	var factory = {};
 
 	factory.get = function(url, success, error) {
+		var now = new Date().getTime();
+
 		// Als er data te vinden is in cache > success functie aanroepen
-		if(localStorage.getItem(url)) {
-			success(localStorage.getItem(url));
+		if(localStorage.getItem(url) !== undefined && localStorage.getItem(url) !== null) {
+			success(JSON.parse(localStorage.getItem(url).data));
 		}
 
 		// Altijd weer nieuwe data ophalen
 		$http.get(url).success(function(data) {
 
-			localStorage.setItem(url, data);
-
-			if(localStorage.getItem(url) !== data) success(data);
+			if(JSON.parse(localStorage.getItem(url)) !== data || now - JSON.parse(localStorage.getItem(url)).timestamp < 3600000) ) {
+				success(data);
+			}
+			localStorage.setItem(url, JSON.stringify({ data: data, timestamp: now }));
 
 		}).error(function(data){
 			error(data);
