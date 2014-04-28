85fa5c9858c4222d75664aedebcdf4cb43a1f367
Olivier Jansen <oli4jansen.nl@gmail.com>
2014-04-28 10:37:42 +0200

    Cache factory opgedeeld in aparte functies

--- www/app/factories/cacheFactory.js
+++ www/app/factories/cacheFactory.js
@@ -5,24 +5,42 @@ app.factory('cacheFactory', function($location, $window, $sce, $q, $http) {
 
 	factory.get = function(url, success, error) {
 		var now = new Date().getTime();
-		var cacheFilled = false;
 
 		// Als er data te vinden is in cache > success functie aanroepen
-		if(localStorage.getItem(url) !== undefined && localStorage.getItem(url) !== null) {
-			success(JSON.parse(localStorage.getItem(url)).data);
+		if(factory.getFromCache(url)) {
+			success(factory.getFromCache(url).data);
 		}
 
 		// Altijd weer nieuwe data ophalen
 		$http.get(url).success(function(data) {
-
-			if(cacheFilled && ((JSON.parse(localStorage.getItem(url)).data !== data) || (now - JSON.parse(localStorage.getItem(url)).timestamp) < 3600000)) {
+			if(factory.dataChanged(url, data)) {
 				success(data);
+				factory.putInCache(url, data);
 			}
-			localStorage.setItem(url, JSON.stringify({ data: data, timestamp: now }));
-
 		}).error(function(data){
 			error(data);
 		});
+	};
+
+	factory.getFromCache = function(key) {
+		if(localStorage.getItem(key) !== undefined && localStorage.getItem(key) !== null) {
+			return JSON.parse(localStorage.getItem(key));
+		}else{
+			return false;
+		}
+	};
+
+	factory.dataChanged = function(key, newData) {
+		var oldData = factory.getFromCache(key);
+		if(oldData && oldData == newData) {
+			return false;
+		}else{
+			return true;
+		}
+	};
+
+	factory.putInCache = function(key, data) {
+		localStorage.setItem(key, data);
 	};
 
 	return factory;
