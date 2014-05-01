de5702fada14f9b691905e8223ba51870ca286b9
Olivier Jansen <oli4jansen.nl@gmail.com>
2014-05-01 16:14:43 +0200

    testing

--- www/app/controllers/startController.js
+++ www/app/controllers/startController.js
@@ -1,16 +1,13 @@
 app.controller("startController", function($scope, $location, $http, $rootScope, loginFactory, $sce){
 
 	$scope.init = function() {
-		var accessTokenFound = false;
-
 		if(localStorage.getItem('babbleAccessToken') !== undefined && localStorage.getItem('babbleAccessToken') !== null && localStorage.getItem('babbleAccessToken') !== 'null') {
 			alert('Cookie gevonden');
 			alert(localStorage.getItem('babbleAccessToken'));
-
-			accessTokenFound = true;
+		}else{
+//			$location.path('/login');
+			alert('Naj');
 		}
-
-		if(!accessTokenFound) $location.path('/login');
 	};
 
 });
